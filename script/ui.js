b7498ca747e874b5d57dc54b2720ec0cdb8ee331
albadr <kaitokitthebest@gmail.com>
2024-08-19 14:24:53 +0300

    reorganize Js + adabting loading-response period behavior + loading-response animations

--- script/ui.js
+++ script/ui.js
@@ -1,10 +1,104 @@
 const copyBtn = document.getElementById("copyBtn");
 const responseBox = document.getElementById("response");
 const sendBtn = document.getElementById("send_button");
+const actionBar = document.getElementById("actionBar");
+const sendButton = document.getElementById("send_button");
+const container = document.getElementById("container");
+const inputBox = document.getElementById("user_input");
 let sentences;
 
+sendButton.addEventListener("click", sendFakeMessage);
+// sendButton.addEventListener("click", sendMessage);
+inputBox.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        // sendMessage();
+        sendFakeMessage();
+    }
+});
 copyBtn.addEventListener("click", copyChat);
 sendBtn.addEventListener("click", bubbleIt);
+
+function sendFakeMessage() {
+    // delete this func
+    function pushResponse() {
+        responseBox.innerHTML = `<p>صف ذا ثنا كم جاد شخص قد سما دم طيبا زد في تقى ضع ظالما:</p>
+<p>  صف: فعل أمر مبني على السكون</p>
+<p> ذا: مفعول به أول منصوب بالالف</p>
+<p>  ثنا: مفعول به ثان منصوب بالفتحة</p>
+<p>  كم: خبرية مبنية على السكون </p>
+<p>  جاد: فعل ماضٍ مبني على الفتح </p>
+<p>  شخص: فاعل مرفوع بالضمة </p>
+<p> قد: حرف تحقيق مبني على السكون</p>
+<p>  سما: فعل ماضٍ مبني على الفتح </p>
+<p> دم: فاعل مرفوع بالضمة</p>
+<p>  طيبا: حال منصوب بالفتحة</p>
+<p>  زد: فعل أمر مبني على السكون</p>
+<p>  في: حرف جر مبني على السكون</p>
+<p>  التقى: اسم مجرور بالكسرة</p>
+<p>  ضع: فعل أمر مبني على السكون</p>
+<p>  ظالما: مفعول به منصوب بالفتحة </p>`;
+    }
+    function disableInputbox(bol) {
+        //if input is true the input box will be disabled
+        inputBox.disabled = bol;
+        if (bol) {
+            inputBox.blur();
+            inputBox.classList.add("disabled");
+        } else {
+            inputBox.classList.remove("disabled");
+            inputBox.focus();
+            inputBox.select();
+        }
+    }
+
+    sendBtn.classList.add("loading");
+    disableInputbox(true);
+
+    // temp function to test the responses for frontend
+    console.log("we are faking a response");
+
+    setTimeout(() => {
+        pushResponse();
+        disableInputbox(false);
+        container.classList.add("response-exists");
+        responseBox.classList.toggle("visible");
+        actionBar.classList.remove("uncopiable");
+        sendBtn.classList.remove("loading");
+    }, 5000);
+}
+
+function sendMessage() {
+    // Clear the input field and disable the button while processing
+    sendButton.classList.add("loading");
+
+    fetch("/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: inputBox.value }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            sendButton.classList.remove("loading");
+            responseBox.innerHTML = marked.parse(data.response);
+            responseBox.classList.add("visible");
+            if (!actionBar.classList.contains("copiable")) {
+                // show the copy button if not shown
+                actionBar.classList.add("copiable");
+            }
+            // Focus the input field and select its content
+            inputBox.focus();
+            inputBox.select();
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            sendButton.classList.remove("loading");
+            responseBox.innerHTML = "حدث خطأ، أعد المحاولة!";
+            responseBox.classList.add("visible");
+        });
+}
 
 function copyChat() {
     if (responseBox.hasChildNodes()) {
@@ -39,8 +133,7 @@ function copyChat() {
 }
 function bubbleIt() {
     if (sendBtn.classList.contains("animate")) {
-        //reset animatoin
-        sendBtn.classList.remove("animate");
+        sendBtn.classList.remove("animate"); //reset animation
     }
     sendBtn.classList.add("animate");
     setTimeout(function () {
